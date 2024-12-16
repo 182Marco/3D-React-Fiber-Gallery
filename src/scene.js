ccfede744de96c01c923a024efa8bad7b48ec1a5
Milza <marcomilza@gmail.com>
2024-12-16 19:44:53 +0100

    animated with useFrame

--- src/scene.js
+++ src/scene.js
@@ -1,7 +1,14 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
 const Scene = () => {
+  const cubeRef = useRef();
+  useFrame(() => {
+    cubeRef.current.rotation.y += 0.01;
+  });
   return (
     <group>
-      <mesh position-x={-2}>
+      <mesh position-x={-2} ref={cubeRef}>
         <boxGeometry arg={[8, 3, 10]} />
         <meshBasicMaterial color="turquoise" wireframe />
       </mesh>
