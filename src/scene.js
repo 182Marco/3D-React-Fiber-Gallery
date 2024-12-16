7d8fa630e68360d3d7b5066d1d564187c2bb7a14
Milza <marcomilza@gmail.com>
2024-12-16 22:40:25 +0100

    added orbit controls

--- src/scene.js
+++ src/scene.js
@@ -1,22 +1,31 @@
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import * as T from "three";
+import { OrbitControls } from "@react-three/drei";
 
 const Scene = () => {
   const cubeRef = useRef();
+  const plane = useRef();
+
   useFrame((state, deltaTime) => {
-    cubeRef.current.rotation.y += deltaTime * 10;
+    cubeRef.current.rotation.y += deltaTime * 0.3;
+    plane.current.rotation.y += deltaTime * 0.33;
   });
+
   return (
-    <group>
-      <mesh position-x={-2} ref={cubeRef}>
-        <boxGeometry arg={[8, 3, 10]} />
-        <meshBasicMaterial color="turquoise" wireframe />
-      </mesh>
-      <mesh position-x={2}>
-        <boxGeometry arg={[2, 10, 6]} />
-        <meshBasicMaterial color="orange" wireframe />
-      </mesh>
-    </group>
+    <>
+      <OrbitControls />
+      <group>
+        <mesh position-x={-2} ref={cubeRef}>
+          <boxGeometry arg={[8, 3, 10]} />
+          <meshBasicMaterial color="turquoise" wireframe />
+        </mesh>
+        <mesh position-x={2} ref={plane}>
+          <planeGeometry arg={[2, 10, 6]} side={T.DoubleSide} />
+          <meshBasicMaterial color="lightgreen" />
+        </mesh>
+      </group>
+    </>
   );
 };
 
