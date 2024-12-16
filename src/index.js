843a58a7644b889efe5e31aa4b86561536a982cb
Milza <marcomilza@gmail.com>
2024-12-16 01:22:35 +0100

    seen group component

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import { Canvas } from "@react-three/fiber";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Canvas>
-    <mesh position={[-4, 0, 0]} rotation-x={Math.PI * 0.5}>
-      <torusKnotGeometry />
-      <meshNormalMaterial />
-    </mesh>
-    <mesh position={[4, 0, 0]} scale={1.5}>
-      <torusKnotGeometry />
-      <meshNormalMaterial />
-    </mesh>
+    <group position={[0, -1, 0]}>
+      <mesh position={[-4, 0, 0]} rotation-x={Math.PI * 0.5}>
+        <torusKnotGeometry />
+        <meshNormalMaterial />
+      </mesh>
+      <mesh position={[4, 0, 0]} scale={1.5}>
+        <torusKnotGeometry />
+        <meshNormalMaterial />
+      </mesh>
+    </group>
   </Canvas>
 );
