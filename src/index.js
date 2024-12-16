9a6e97a5aa2fe3b1c2b8593e933862f531c89d11
Milza <marcomilza@gmail.com>
2024-12-16 01:33:49 +0100

    Canvas with fiber always inherits width from parent

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import { Canvas } from "@react-three/fiber";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Canvas>
-    <group position={[0, -1, 0]}>
-      <mesh position={[-4, 0, 0]} rotation-x={Math.PI * 0.5}>
-        <torusKnotGeometry />
-        <meshNormalMaterial />
-      </mesh>
-      <mesh position={[4, 0, 0]} scale={1.5}>
-        <torusKnotGeometry />
-        <meshNormalMaterial />
-      </mesh>
-    </group>
-  </Canvas>
+  <div className="box">
+    <Canvas>
+      <group position={[0, -1, 0]}>
+        <mesh position={[-4, 0, 0]} rotation-x={Math.PI * 0.5}>
+          <torusKnotGeometry />
+          <meshNormalMaterial />
+        </mesh>
+        <mesh position={[4, 0, 0]} scale={1.5}>
+          <torusKnotGeometry />
+          <meshNormalMaterial />
+        </mesh>
+      </group>
+    </Canvas>
+  </div>
 );
