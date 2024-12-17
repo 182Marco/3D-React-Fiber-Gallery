a2750770003613e257352886c27a5a2c983dc7ee
Milza <marcomilza@gmail.com>
2024-12-17 15:54:16 +0100

    2nd way to change the bg

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ import "./styles.css";
 import { Canvas } from "@react-three/fiber";
 import { Scene } from "./scene";
 
+const createCanvasHandler = (state) => state.gl.setClearColor("red", 0.1);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div className="box">
     <Canvas
-      gl={{ alpha: true }}
+      // gl={{ alpha: true }}
       camera={{
         fov: 75,
         near: 0.1,
         far: 100,
         position: [6, 0, -5],
       }}
+      onCreated={createCanvasHandler}
     >
       <group position={[0, 0, -3]}>
         <Scene />
