efc480af7844d85b4fdb6af5a12b720e51e79978
Milza <marcomilza@gmail.com>
2024-12-18 23:36:45 +0100

    triangle with texture

--- src/custom.js
+++ src/custom.js
@@ -1,18 +1,30 @@
+import { useLoader } from "@react-three/fiber";
 import * as THREE from "three";
 
 const Custom = () => {
+  const texture = useLoader(THREE.TextureLoader, "./img/1.png");
+
   const positionArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
+
+  const uvCoordinates = new Float32Array([0, 0, 0, 1, 0]);
+
   return (
     <mesh>
       <bufferGeometry>
         <bufferAttribute
           attach="attributes-position"
-          count={3}
+          count={positionArray.length / 3}
           itemSize={3}
           array={positionArray}
         />
+        <bufferAttribute
+          attach="attributes-uv"
+          count={uvCoordinates.length / 2}
+          itemSize={2}
+          array={uvCoordinates}
+        />
       </bufferGeometry>
-      <meshBasicMaterial color="orange" side={THREE.DoubleSide} />
+      <meshBasicMaterial side={THREE.DoubleSide} map={texture} />
     </mesh>
   );
 };
