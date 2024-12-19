d8b285e6facbb08f8db46233032c4dfd0ec791e4
Milza <marcomilza@gmail.com>
2024-12-19 23:46:48 +0100

    with faster sintax

--- src/componets/model/model.hook.js
+++ src/componets/model/model.hook.js
@@ -1,8 +1,10 @@
+import { useGLTF } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const useModel = () => ({
-  model: useLoader(GLTFLoader, "./model/dog.glb"),
+  //model: useLoader(GLTFLoader, "./model/dog.glb"),
+  model: useGLTF("./model/dog.glb"),
 });
 
 export { useModel };
