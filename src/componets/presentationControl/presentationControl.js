7bf6b71ada99ef303db5805003264e2fa79c4625
Milza <marcomilza@gmail.com>
2024-12-30 19:11:54 +0100

    snap attribute

--- src/componets/presentationControl/presentationControl.js
+++ src/componets/presentationControl/presentationControl.js
@@ -9,6 +9,8 @@ const PresentationControl = () => {
       global
       polar={[-Math.PI / 3, Math.PI / 3]}
       azimuth={[-Math.PI / 3, Math.PI / 3]}
+      config={{ mass: 2, tensio: 5000 }}
+      snap={{ mass: 2, tensio: 5000 }}
     >
       <mesh>
         <boxGeometry args={[4, 4, 4]} />
