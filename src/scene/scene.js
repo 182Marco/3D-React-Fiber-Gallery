170cb8ab4989a65ad88a5662eb2642e3762d316a
Milza <marcomilza@gmail.com>
2025-01-04 00:21:32 +0100

    initialized

--- src/scene/scene.js
+++ src/scene/scene.js
@@ -1,76 +1,18 @@
 import * as C from "../componets";
 import * as D from "@react-three/drei";
-import { useRef } from "react";
+import { useState } from "react";
 
 const Scene = () => {
-  const orangeMesh = useRef();
+  const [play, setPlay] = useState(false);
+
   return (
     <>
+      {play && <D.PositionalAudio url="./sound/sound.mp3" autoplay loop />}
       <D.OrbitControls position={[0, 1, 0]} />
-      <mesh position={[1, 0, 0]} ref={orangeMesh}>
+      <mesh onClick={() => setPlay(pv => !pv)}>
         <boxGeometry />
         <meshBasicMaterial color="orange" />
-        <D.Html
-          position={[0, 0.5, 0.5]}
-          wrapperClass="text-in-canvas"
-          distanceFactor={5} // to add prospective to text when zoom in and out
-          // occlude={[orangeMesh]} ---> not working --> don't know why
-        >
-          R3F
-        </D.Html>
       </mesh>
-      <mesh position={[-1, 0, 0]}>
-        <boxGeometry />
-        <meshBasicMaterial color="violet" />
-      </mesh>
-      {/* 
-      <D.Text
-        fontSize={0.6}
-        color="turquoise"
-        font="./fonts/Quicksand-Medium.ttf"
-        position={[5, 1.5, 0]}
-        rotation-y={Math.PI / 16}
-        maxWidth={5} // in axes units
-        textAlign="center"
-      >
-        This is the text branch
-      </D.Text>
-      <D.Center>
-        <D.Float speed={3} floatIntensity={10}>
-          <D.Text3D
-            bevelEnabled
-            bevelSegments={60}
-            font="./fonts/Quicksand Medium_Regular.json"
-            letterSpacing={0.3}
-            height={1.3}
-            size={3}
-            position={[0, -1.5, 0]}
-          >
-            3D
-            <meshNormalMaterial />
-          </D.Text3D>
-        </D.Float>
-      </D.Center> */}
-      {/* <C.PivotControl /> */}
-      {/* <C.TransformControl /> */}
-      {/* <C.ScrollControl /> */}
-      {/*  <C.PresentationControl />
-     <Environment background files="./envMap/1.hdr" />
-      <C.CameraControl /> 
-       <C.OrbitControl /> 
-    
-      <CubeCamera resolution={1024}>
-        {(texture) => (
-          <mesh>
-            <sphereGeometry args={[1, 64, 64]} />
-            <meshStandardMaterial
-              envMap={texture}
-              roughness={0}
-              metalness={0.9}
-            />
-          </mesh>
-        )}
-      </CubeCamera> */}
     </>
   );
 };
