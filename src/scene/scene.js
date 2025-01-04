5454abe68e7ee76ea5a3e00eb5306b982ebee5c2
Milza <marcomilza@gmail.com>
2025-01-04 13:27:49 +0100

    initialized

--- src/scene/scene.js
+++ src/scene/scene.js
@@ -1,19 +1,25 @@
 import * as C from "../componets";
 import * as D from "@react-three/drei";
-import { useState } from "react";
 
 const Scene = () => {
-  const [play, setPlay] = useState(false);
-
   return (
     <>
-      {play && (
-        <D.PositionalAudio url="./sound/sound.mp3" autoplay loop distance={5} />
-      )}
-      <D.OrbitControls position={[0, 1, 10]} />
-      <mesh onClick={() => setPlay(pv => !pv)}>
+      <ambientLight />
+      <D.OrbitControls position={[0, 3, 3]} />
+      <D.Environment background files="./envMap/envMap.hdr" />
+      <mesh>
         <boxGeometry />
-        <meshBasicMaterial color="orange" />
+        <meshBasicMaterial color="red" />
+      </mesh>
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position-y={-1}>
+        <boxGeometry args={[6, 6, 1]} />
+        <D.MeshReflectorMaterial
+          resolution={512}
+          color="grey"
+          blur={[1000, 100]}
+          mixBlur={1}
+          mirror={0.8} // reflect evironment map
+        />
       </mesh>
     </>
   );
