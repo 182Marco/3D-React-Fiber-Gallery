b9cbaf6e51d4429e0e87d85b75cdbfc17ad733c8
Milza <marcomilza@gmail.com>
2025-01-04 15:17:46 +0100

    animated move with T function

--- src/scene/scene.js
+++ src/scene/scene.js
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 import * as C from "../componets";
 import * as D from "@react-three/drei";
+import * as THREE from "three";
 
 const Scene = () => {
+  const cubeRef = useRef();
   const [hover, setHover] = useState(false);
   D.useCursor(hover);
+
+  useFrame(
+    () =>
+      (cubeRef.current.position.x = THREE.MathUtils.lerp(
+        cubeRef.current.position.x,
+        3,
+        0.001
+      ))
+  );
   return (
     <>
-      <ambientLight />
+      <ambientLight intensity={1} />
       <D.OrbitControls position={[0, 3, 3]} rotation={[-Math.PI, 0, 0]} />
       <mesh
         rotation={[0, 0, Math.PI]}
@@ -16,15 +28,24 @@ const Scene = () => {
         onPointerLeave={() => setHover(false)}
       >
         <planeGeometry args={[5, 5, 32, 32]} />
-        <D.MeshDistortMaterial color="white" distort={0.3} speed={3} wireframe>
+        <D.MeshDistortMaterial
+          color="white"
+          distort={hover ? 0.3 : 0}
+          speed={3}
+          wireframe
+        >
           <D.GradientTexture
             colors={["red", "blue", "yellow"]}
             stops={[0, 0.5, 1]}
           />
         </D.MeshDistortMaterial>
       </mesh>
+      <mesh position={[0, -4, 0]} ref={cubeRef}>
+        <boxGeometry args={[1, 1, 1, 4, 4, 4]} />
+        <meshStandardMaterial color="green" wireframe />
+      </mesh>
       {/* <mesh>
-        <boxGeometry args={[1, 1, 1, 32, 32, 32]} />
+        <boxGeometry args={[1, 1, 1, 16, 32, 32]} />
         <D.MeshWobbleMaterial color="red" factor={3} speed={0.3} wireframe />
         gelatinus material
       </mesh> */}
