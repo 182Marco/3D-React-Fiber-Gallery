91cba42584308ac9fbac10c468c849589d0f0702
Milza <marcomilza@gmail.com>
2025-01-06 18:53:07 +0100

    blend with animation

--- src/scene/scene.js
+++ src/scene/scene.js
@@ -1,13 +1,20 @@
-import { useState } from "react";
 import * as C from "../componets";
 import * as D from "@react-three/drei";
+import * as M from "maath";
+import * as R from "react";
+import * as T from "@react-three/fiber";
 import * as THREE from "three";
 
 const Scene = () => {
   const model = D.useGLTF("./model/1.glb");
   const texture = D.useTexture("./texture/1.png");
+  const portalMaterialRef = R.useRef();
 
-  const [active, setActive] = useState(false);
+  const [active, setActive] = R.useState(false);
+
+  T.useFrame(() => {
+    M.easing.damp(portalMaterialRef.current, "blend", active ? 1 : 0);
+  });
 
   return (
     <>
@@ -26,7 +33,7 @@ const Scene = () => {
         radius={0.1}
         onDoubleClick={() => setActive(pv => !pv)}
       >
-        <D.MeshPortalMaterial blend={active ? 1 : 0}>
+        <D.MeshPortalMaterial ref={portalMaterialRef}>
           <primitive object={model.scene} scale={0.6} position={[0, 0.6, 0]} />
           <mesh>
             <sphereGeometry args={[5, 64, 64]} />
