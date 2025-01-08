190522d39bc5b860b80124fce5be4f7c0bce85a9
Milza <marcomilza@gmail.com>
2025-01-08 18:52:45 +0100

    first animation working

--- src/scene/scene.js
+++ src/scene/scene.js
@@ -1,11 +1,22 @@
+import { a, useSpring } from "@react-spring/three";
 import * as C from "../components";
 
 const Scene = () => {
+  const { color, x, y } = useSpring({
+    from: { color: "hotpink", y: -2, x: -2 },
+    to: {
+      color: "yellow",
+      x: 0,
+    },
+  });
+
   return (
-    <mesh>
-      <boxGeometry />
-      <meshBasicMaterial color="orange" />
-    </mesh>
+    <>
+      <a.mesh position-x={x} position-y={y}>
+        <boxGeometry />
+        <meshBasicMaterial color={color} />
+      </a.mesh>
+    </>
   );
 };
 
