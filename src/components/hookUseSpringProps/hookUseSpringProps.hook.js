cf7a294fda735b77f91d3991b606ba3535fc3d82
Milza <marcomilza@gmail.com>
2025-01-09 18:34:29 +0100

    separated logic in HookUseSpringProps compoent

---
+++ src/components/hookUseSpringProps/hookUseSpringProps.hook.js
@@ -0,0 +1,46 @@
+import { a, useSpring } from "@react-spring/three";
+import { useState } from "react";
+
+const useHookUseSpringProps = () => {
+  const [clicked, setClick] = useState(false);
+  let n = 0;
+  const { color, x, y } = useSpring({
+    from: { color: "pink", y: -2, x: -2 },
+    to: [
+      {
+        color: "yellow",
+        x: 2,
+      },
+      {
+        color: "green",
+        y: 2,
+      },
+      {
+        color: "red",
+        x: -2,
+      },
+      {
+        color: "violet",
+        y: -2,
+      },
+    ],
+    loop: () => n++ < 2, // assign fn to assign to loop dynamically, based on other values if needed.
+    delay: 2000,
+    // reverse: clicked,
+    //reset: clicked,
+    pause: clicked,
+    onStart: () => console.log("start"),
+    onPause: () => console.log("pause"),
+    onResume: () => console.log("resume"),
+    onRest: () => console.log("rest"),
+  });
+
+  return {
+    color,
+    x,
+    y,
+    setClick,
+  };
+};
+
+export { useHookUseSpringProps };
--- src/components/hookUseSpringProps/hookUseSpringProps.js
+++ src/components/hookUseSpringProps/hookUseSpringProps.js
@@ -1,44 +1,16 @@
-import { a, useSpring } from "@react-spring/three";
-import { useState } from "react";
+import { a } from "@react-spring/three";
+import { useHookUseSpringProps } from "./hookUseSpringProps.hook";
 
 const HookUseSpringProps = () => {
-  const [clicked, setClick] = useState(false);
-  let n = 0;
-  const { color, x, y } = useSpring({
-    from: { color: "pink", y: -2, x: -2 },
-    to: [
-      {
-        color: "yellow",
-        x: 2,
-      },
-      {
-        color: "green",
-        y: 2,
-      },
-      {
-        color: "red",
-        x: -2,
-      },
-      {
-        color: "violet",
-        y: -2,
-      },
-    ],
-    loop: () => n++ < 2, // assign fn to assign to loop dynamically, based on other values if needed.
-    delay: 2000,
-    // reverse: clicked,
-    //reset: clicked,
-    pause: clicked,
-    onStart: () => console.log("start"),
-    onPause: () => console.log("pause"),
-    onResume: () => console.log("resume"),
-    onRest: () => console.log("rest"),
-  });
-
+  const u = useHookUseSpringProps();
   return (
-    <a.mesh position-x={x} position-y={y} onClick={() => setClick(pv => !pv)}>
+    <a.mesh
+      position-x={u.x}
+      position-y={u.y}
+      onClick={() => u.setClick(pv => !pv)}
+    >
       <boxGeometry />
-      <a.meshBasicMaterial color={color} />
+      <a.meshBasicMaterial color={u.color} />
     </a.mesh>
   );
 };
