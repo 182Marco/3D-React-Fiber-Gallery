c63fabe7518a16799859674d0e35521786ba2b75
Milza <marcomilza@gmail.com>
2025-01-13 21:25:14 +0100

    addTorque method used

--- src/components/methodsOfTheRigidBody/methodsOfTheRigidBody.hook.js
+++ src/components/methodsOfTheRigidBody/methodsOfTheRigidBody.hook.js
@@ -6,10 +6,11 @@ const useMethodsOfTheRigidBody = () => {
   const fistMeshClickHandler = () => {
     //  firstMesh.current.addForce({ x: 0, y: 10, z: 0 });
     // firstMesh.current.applyImpulse({ x: -3, y: 5, z: 4.5 });
-    firstMesh.current.applyImpulseAtPoint(
-      { x: -1, y: 2, z: 1.7 },
-      { x: 2, y: 0, z: 0 }, // 2nd argument is where to appy the force
-    );
+    // firstMesh.current.applyImpulseAtPoint(
+    //   { x: -1, y: 2, z: 1.7 },
+    //   { x: 2, y: 0, z: 0 }, // 2nd argument is where to appy the force
+    // );
+    firstMesh.current.addTorque({ x: 0, y: 33, z: 0 });
   };
 
   return { firstMesh, fistMeshClickHandler };
