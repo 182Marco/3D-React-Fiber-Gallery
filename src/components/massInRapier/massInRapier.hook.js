6c6095d0d3226b2a979090094df58e4421cbc00f
Milza <marcomilza@gmail.com>
2025-01-15 21:43:36 +0100

    mass and power in vector affect movements

--- src/components/massInRapier/massInRapier.hook.js
+++ src/components/massInRapier/massInRapier.hook.js
@@ -5,18 +5,11 @@ const useMassInRapier = () => {
   const secondMesh = useRef();
 
   const fistMeshClickHandler = () => {
-    //  firstMesh.current.addForce({ x: 0, y: 10, z: 0 });
-    // firstMesh.current.applyImpulse({ x: -3, y: 5, z: 4.5 });
-    // firstMesh.current.addTorque({ x: 0, y: 33, z: 0 });
-    // firstMesh.current.applyTorqueImpulse({ x: 0, y: 4, z: 0 });
-    firstMesh.current.applyImpulseAtPoint(
-      { x: -1, y: 2, z: 1.7 },
-      { x: 2, y: 0, z: 0 }, // 2nd argument is where to appy the force
-    );
+    firstMesh.current.applyImpulse({ x: -100, y: 0, z: 0 });
   };
 
   const secondMeshClickHandler = () => {
-    secondMesh.current.applyTorqueImpulse({ x: 0, y: 4, z: 0 });
+    secondMesh.current.applyImpulse({ x: 100, y: 0, z: 0 });
   };
 
   return {
--- src/components/massInRapier/massInRapier.js
+++ src/components/massInRapier/massInRapier.js
@@ -14,15 +14,9 @@ const MassInRapier = () => {
         <R.Debug />
         <R.RigidBody
           colliders="cuboid"
-          position={[0, 3, 0]}
-          //  rotation={[2, 3, 4]}
+          position={[1.5, 3, 0]}
           ref={u.firstMesh}
-          onCollisionEnter={() => console.log(`collision enter`)}
-          onCollisionExit={() => console.log(`collision exit`)}
-          onSleep={() => console.log(`sleeping`)}
-          onWake={() => console.log(`wake`)}
-          gravityScale={0.2} // can also move up with minus value
-          // restitution={2} // Adds floor restitution (0), gives 2 / 2 = 1-> returns to the fall height.
+          scale={4}
         >
           <mesh castShadow onClick={u.fistMeshClickHandler}>
             <boxGeometry />
@@ -30,34 +24,16 @@ const MassInRapier = () => {
           </mesh>
         </R.RigidBody>
         <R.RigidBody
-          colliders="ball"
-          position={[3, 3, 1]}
-          gravityScale={0.2} // can also move up with minus value
-          restitution={1} // If restitution is 1, like the floor, you will have infinite bouncing.
-        >
-          <mesh castShadow onClick={u.fistMeshClickHandler}>
-            <sphereGeometry />
-            <meshStandardMaterial color="#32FF32" />
-          </mesh>
-        </R.RigidBody>
-        <R.RigidBody
           colliders="cuboid"
-          position={[-2, 3.5, 2]}
-          rotation={[1, 4, 1]}
+          position={[-1.5, 3, 0]}
           ref={u.secondMesh}
-          restitution={-1}
-          friction={0} // Less friction with the floor means it will take longer to stop rotating
         >
           <mesh castShadow onClick={u.secondMeshClickHandler}>
             <boxGeometry />
             <meshStandardMaterial color="#CC2941" />
           </mesh>
         </R.RigidBody>
-        <R.RigidBody
-          type="fixed"
-          restitution={1}
-          friction={0} // 0 + 0 = 0 so infinite rotation or impulse
-        >
+        <R.RigidBody type="fixed">
           <mesh position-y={-1} rotation-x={-Math.PI * 0.5} receiveShadow>
             <boxGeometry args={[8, 8, 0.35]} />
             <meshStandardMaterial color="#C7CAC7" />
