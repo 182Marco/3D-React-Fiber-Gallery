77fb2e08d9adb5577f57e0a566456448fd42907e
Milza <marcomilza@gmail.com>
2025-01-20 00:45:12 +0100

    less code

--- src/components/overlayText/overlayText.js
+++ src/components/overlayText/overlayText.js
@@ -6,7 +6,7 @@ const OverlayText = props =>
   !!props?.txt?.length && (
     <D.Scroll className="w100-scroll" html>
       {props.txt.map((e, i) => (
-        <section className={(i + 1) % 2 ? "right" : ""} key={i}>
+        <section className={i % 2 ? "" : "right"} key={i}>
           <div dangerouslySetInnerHTML={{ __html: U.purify(e) }} />
         </section>
       ))}
