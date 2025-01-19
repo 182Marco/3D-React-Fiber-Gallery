1219c8d93608e720b2fbe0d67180c32b9466ba00
Milza <marcomilza@gmail.com>
2025-01-20 00:53:10 +0100

    added logic to overlay text

--- src/components/overlayText/overlayText.js
+++ src/components/overlayText/overlayText.js
@@ -1,16 +1,21 @@
 import * as D from "@react-three/drei";
 import * as U from "../../utils";
+import { useOverlayText } from "./OverlayText.hook";
 import "./styles.scss";
 
-const OverlayText = props =>
-  !!props?.txt?.length && (
-    <D.Scroll className="w100-scroll" html>
-      {props.txt.map((e, i) => (
-        <section className={i % 2 ? "" : "right"} key={i}>
-          <div dangerouslySetInnerHTML={{ __html: U.purify(e) }} />
-        </section>
-      ))}
-    </D.Scroll>
+const OverlayText = props => {
+  const h = useOverlayText();
+  return (
+    !!props?.txt?.length && (
+      <D.Scroll className="w100-scroll" html>
+        {props.txt.map((e, i) => (
+          <section className={i % 2 ? "" : "right"} key={i}>
+            <div dangerouslySetInnerHTML={{ __html: U.purify(e) }} />
+          </section>
+        ))}
+      </D.Scroll>
+    )
   );
+};
 
 export { OverlayText };
