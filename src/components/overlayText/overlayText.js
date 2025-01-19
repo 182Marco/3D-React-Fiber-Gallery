668b5c3c7a74dfc085b703c6753d886125751570
Milza <marcomilza@gmail.com>
2025-01-19 03:04:57 +0100

    text almost good

--- src/components/overlayText/overlayText.js
+++ src/components/overlayText/overlayText.js
@@ -4,15 +4,13 @@ import { useOverlayText } from "./overlayText.hook";
 
 const OverlayText = props => {
   const h = useOverlayText();
-  const _props = { txt: "<h1> ciao a tutti monelli !</h1>" };
+  const _props = {
+    txt: "<h1> ciao a tutti monelli ! <br/> cioa <br/> cioa<br/> cioa</h1>",
+  };
   return (
     <D.Scroll html>
       <main className="text-box">
-        <section>
-          <div>
-            <div dangerouslySetInnerHTML={{ __html: _props.txt }} />
-          </div>
-        </section>
+        <section dangerouslySetInnerHTML={{ __html: _props.txt }} />
       </main>
     </D.Scroll>
   );
