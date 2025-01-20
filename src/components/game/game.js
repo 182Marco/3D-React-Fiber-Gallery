ae279e65e1af1d4960bc1c8d5647e5b242b5def8
Milza <marcomilza@gmail.com>
2025-01-21 00:08:26 +0100

    2 meshes in single rapier rigidbody

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -7,27 +7,36 @@ const Game = () => {
   const u = useGame();
 
   return (
-    <Suspense>
-      <R.Physics debug>
+    <R.Physics debug gravity={[0, -9.8, 0]}>
+      {/* without <Suspense> you can't see colliders  */}
+      <Suspense>
         <ambientLight intensity={0.5} />
         <directionalLight position={[-10, 10, 0]} intensity={0.4} />
         <D.OrbitControls />
-        <R.RigidBody position={[0, 5, 0]}>
-          <D.Sphere>
+        <R.RigidBody position={[0, 5, 0]} colliders={false}>
+          <R.BallCollider args={[1]} position={[0, 1, 0]} />
+          <D.Sphere position-y={1}>
             <meshStandardMaterial color="hotpink" />
           </D.Sphere>
-        </R.RigidBody>
-        <R.RigidBody position={[3, 5, 0]}>
+          <R.CuboidCollider args={[0.5, 0.5, 0.5]} />
           <D.Box>
             <meshStandardMaterial color="royalblue" />
           </D.Box>
         </R.RigidBody>
-        <R.RigidBody type="fixed">
+        <R.RigidBody position={[3, 5, 0]}></R.RigidBody>
+        <R.RigidBody position={[-4, 5, 0]} colliders="hull">
+          <D.Torus>
+            <meshStandardMaterial color="orange" />
+          </D.Torus>
+        </R.RigidBody>
+        <R.RigidBody type="fixed" restitution={1}>
           <D.Box position={[0, 0, 0]} args={[10, 1, 10]} />
           <meshStandardMaterial color="springgreen" />
         </R.RigidBody>
-      </R.Physics>
-    </Suspense>
+        {/* Aggiungi il componente Debug per una visualizzazione dettagliata */}
+        <R.Debug />
+      </Suspense>
+    </R.Physics>
   );
 };
 
