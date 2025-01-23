457847af88cd0303f8987eb43c1608b4f12d0f60
Milza <marcomilza@gmail.com>
2025-01-24 00:13:33 +0100

    moved utils

--- src/components/explode/explode.js
+++ src/components/explode/explode.js
@@ -1,11 +1,16 @@
-import { useExplode } from "./explode.hook";
+import * as D from "@react-three/drei";
 import * as C from "../../components";
+import { useExplode } from "./explode.hook";
 
 const Explode = () => {
   const h = useExplode();
   return (
     <>
-      <C.Heart />
+      <D.Environment preset="sunset" background blur={0.4} />
+      <D.OrbitControls enableZoom={false} />
+      <D.Float floatIntensity={2} speed={3}>
+        <C.Heart />
+      </D.Float>
     </>
   );
 };
