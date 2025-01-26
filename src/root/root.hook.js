98c710316ab2b9e2e5bda8d17bd4b5b556ebd840
Milza <marcomilza@gmail.com>
2025-01-26 14:53:23 +0100

    updated root hook

--- src/root/root.hook.js
+++ src/root/root.hook.js
@@ -1,7 +1,14 @@
+import * as L from "leva";
 import * as U from "./utils";
 
 const useRoot = () => {
-  return {};
+  const options = Object.keys(U.projects);
+
+  const { project } = L.useControls({
+    project: { value: U.projects.Home3d, options },
+  });
+
+  return { project };
 };
 
 export { useRoot };
