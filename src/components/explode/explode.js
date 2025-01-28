7b3f7303f17c877e8bb92beec3ce826041def805
Milza <marcomilza@gmail.com>
2025-01-28 10:05:46 +0100

    updated explode component

--- src/components/explode/explode.js
+++ src/components/explode/explode.js
@@ -9,8 +9,9 @@ const Explode = () => {
       <D.Environment preset="sunset" background blur={0.4} />
       <D.OrbitControls enableZoom={false} />
       <D.Float floatIntensity={2} speed={3}>
-        <C.Heart visible={h.item === h.heart} />
-        <C.Banana visible={h.item === h.banana} />
+        <C.Heart visible={h.isSelected(h.heart)} scale={0.7} />
+        <C.Banana visible={h.isSelected(h.banana)} scale={0.25} />
+        <C.Bird visible={h.isSelected(h.bird)} scale={1.8} />
       </D.Float>
     </>
   );
