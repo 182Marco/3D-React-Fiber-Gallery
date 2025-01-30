9e6348652d330a84f8d30aba39c96725570909b1
Milza <marcomilza@gmail.com>
2025-01-30 23:46:22 +0100

    fallback on suspense

--- src/root/getComponents/getComponents.js
+++ src/root/getComponents/getComponents.js
@@ -15,7 +15,7 @@ const GetComponents = projName => {
       );
     case U.projects.Dog:
       return (
-        <R.Suspense>
+        <R.Suspense fallback={C.Spinner}>
           <C.Dog />
         </R.Suspense>
       );
