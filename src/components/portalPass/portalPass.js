f3b3b5eb2b5bdb22b86e8daf62ac1502d3e9b272
Milza <marcomilza@gmail.com>
2025-01-31 18:10:53 +0100

    first fragment of portalpass proj on scene

--- src/components/portalPass/portalPass.js
+++ src/components/portalPass/portalPass.js
@@ -1,8 +1,14 @@
+import * as D from "@react-three/drei";
+
 const PortalPass = () => {
   return (
     <>
-      <boxGeometry />
-      <meshBasicMaterial />
+      <D.OrbitControls makeDefault />
+      <ambientLight intensity={1} />
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial color="green" />
+      </mesh>
     </>
   );
 };
