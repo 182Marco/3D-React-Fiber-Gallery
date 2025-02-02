56730d2aa977114cbe899fab42fad6ed6049a161
Milza <marcomilza@gmail.com>
2025-02-02 23:52:50 +0100

    created dynamic card for monster

--- src/components/portalPass/portalPass.js
+++ src/components/portalPass/portalPass.js
@@ -1,6 +1,5 @@
 import * as D from "@react-three/drei";
 import { usePortalPass } from "./portalPass.hook";
-import * as THREE from "three";
 import * as C from "./components";
 
 const PortalPass = () => {
@@ -8,17 +7,9 @@ const PortalPass = () => {
   return (
     <>
       <D.OrbitControls makeDefault />
-      <D.RoundedBox args={[2, 3, 0.1]}>
-        <D.MeshPortalMaterial side={THREE.DoubleSide}>
-          <D.Environment preset="sunset" />
-          <ambientLight intensity={1} />
-          <C.Fish scale={0.55} position-y={-0.7} />
-          <mesh>
-            <sphereGeometry args={[10, 64, 64]} />
-            <meshStandardMaterial map={h.map} side={THREE.BackSide} />
-          </mesh>
-        </D.MeshPortalMaterial>
-      </D.RoundedBox>
+      <C.MonsterStage>
+        <C.Fish scale={0.6} position-y={-1} />
+      </C.MonsterStage>
     </>
   );
 };
