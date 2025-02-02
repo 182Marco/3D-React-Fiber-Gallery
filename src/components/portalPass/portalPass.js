dbf5ca51a5c739d451d7c342952bbe7dc6655ac1
Milza <marcomilza@gmail.com>
2025-02-02 18:37:14 +0100

    added fish

--- src/components/portalPass/portalPass.js
+++ src/components/portalPass/portalPass.js
@@ -1,6 +1,7 @@
 import * as D from "@react-three/drei";
 import { usePortalPass } from "./portalPass.hook";
 import * as THREE from "three";
+import * as C from "./components";
 
 const PortalPass = () => {
   const h = usePortalPass();
@@ -9,6 +10,7 @@ const PortalPass = () => {
       <D.OrbitControls makeDefault />
       <ambientLight intensity={0.5} />
       <D.Environment preset="sunset" />
+      <C.Fish />
       <mesh>
         <sphereGeometry args={[10, 64, 64]} />
         <meshStandardMaterial map={h.map} side={THREE.BackSide} />
