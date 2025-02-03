d4adf72b11f59576471ad1359cd2ef2462393db6
Milza <marcomilza@gmail.com>
2025-02-03 13:55:55 +0100

    added written presentation to monsters

--- src/components/portalPass/portalPass.js
+++ src/components/portalPass/portalPass.js
@@ -13,16 +13,24 @@ const PortalPass = () => {
           texture="textures/LavaWorld.jpg"
           position-x={-2.5}
           rotation-y={Math.PI / 8}
+          name="Dragon"
+          color="#df8252"
         >
-          <C.Dragon scale={0.6} position-y={-1} />
+          <C.Dragon scale={0.46} position-y={-0.8} />
         </C.MonsterStage>
-        <C.MonsterStage texture="textures/waterEnviron.jpg">
+        <C.MonsterStage
+          texture="textures/waterEnviron.jpg"
+          name="Fish King"
+          color="#50b8cd"
+        >
           <C.Fish scale={0.6} position-y={-1} />
         </C.MonsterStage>
         <C.MonsterStage
           texture="textures/cactusForest.jpg"
           position-x={2.5}
           rotation-y={-Math.PI / 8}
+          name="Cactoro"
+          color="#80aa3f"
         >
           <C.Cactoro scale={0.4} position-y={-0.7} />
         </C.MonsterStage>
