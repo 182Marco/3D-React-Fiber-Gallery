802e74662c32297508c2249bbd42900f8f755765
Milza <marcomilza@gmail.com>
2025-02-03 13:23:20 +0100

    updated monsrer positions

--- src/components/portalPass/portalPass.js
+++ src/components/portalPass/portalPass.js
@@ -9,22 +9,22 @@ const PortalPass = () => {
     <>
       <D.OrbitControls makeDefault />
       <group scale={1.2}>
+        <C.MonsterStage
+          texture="textures/LavaWorld.jpg"
+          position-x={-2.5}
+          rotation-y={Math.PI / 8}
+        >
+          <C.Dragon scale={0.6} position-y={-1} />
+        </C.MonsterStage>
         <C.MonsterStage texture="textures/waterEnviron.jpg">
           <C.Fish scale={0.6} position-y={-1} />
         </C.MonsterStage>
         <C.MonsterStage
           texture="textures/cactusForest.jpg"
-          position-x={-2.5}
-          rotation-y={Math.PI / 8}
-        >
-          <C.Cactoro scale={0.6} position-y={-1} />
-        </C.MonsterStage>
-        <C.MonsterStage
-          texture="textures/LavaWorld.jpg"
           position-x={2.5}
           rotation-y={-Math.PI / 8}
         >
-          <C.Dragon scale={0.6} position-y={-1} />
+          <C.Cactoro scale={0.4} position-y={-0.7} />
         </C.MonsterStage>
       </group>
     </>
