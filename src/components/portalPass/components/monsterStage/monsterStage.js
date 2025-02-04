01fd702bcb855f95b487e7195e107391abec5e5d
Milza <marcomilza@gmail.com>
2025-02-04 20:45:41 +0100

    added mosterStage

---
+++ src/components/portalPass/components/monsterStage/monsterStage.js
@@ -0,0 +1,52 @@
+import * as THREE from "three";
+import * as R from "react";
+import * as D from "@react-three/drei";
+import { useMonsterStage } from "./monsterStage.hook";
+
+const MonsterStage = ({
+  children,
+  active,
+  color,
+  hover,
+  name,
+  setActive,
+  setIsHover,
+  texture,
+  ...props
+}) => {
+  const h = useMonsterStage(texture, active, name);
+
+  return (
+    <group {...props}>
+      <D.Text
+        font="./fonts/Caprasimo-Regular.ttf"
+        fontSize={0.3}
+        position={[0, -1.3, 0.051]}
+        anchorY="bottom"
+      >
+        {name}
+        <meshBasicMaterial color={color} toneMapped={false} />
+      </D.Text>
+      <D.RoundedBox
+        args={[2, 3, 0.1]}
+        onClick={e => e.stopPropagation()}
+        onDoubleClick={() => setActive(active === name ? null : name)}
+        onPointerEnter={() => setIsHover(name)}
+        onPointerLeave={() => setIsHover(null)}
+        name={name}
+      >
+        <D.MeshPortalMaterial side={THREE.DoubleSide} ref={h.portalMaterialRef}>
+          <D.Environment preset="sunset" />
+          <ambientLight intensity={1} />
+          {children}
+          <mesh>
+            <sphereGeometry args={[10, 64, 64]} />
+            <meshStandardMaterial map={h.loadedTexture} side={THREE.BackSide} />
+          </mesh>
+        </D.MeshPortalMaterial>
+      </D.RoundedBox>
+    </group>
+  );
+};
+
+export { MonsterStage };
