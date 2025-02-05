7a67f48cdd9edb7f54b556e9976e73cf50b7b051
Milza <marcomilza@gmail.com>
2025-02-05 22:22:12 +0100

    updated portal pass render

--- src/components/portalPass/portalPass.js
+++ src/components/portalPass/portalPass.js
@@ -1,5 +1,6 @@
-import * as C from "./components";
 import * as D from "@react-three/drei";
+import * as C from "./components";
+import * as V from "./environmentVars";
 import { usePortalPass } from "./portalPass.hook";
 
 const PortalPass = () => {
@@ -7,38 +8,48 @@ const PortalPass = () => {
 
   return (
     <>
-      <D.CameraControls ref={h.cameraRef} />
-      <group scale={2.4}>
+      <D.CameraControls
+        ref={h.cameraRef}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={-Math.PI / 6}
+      />
+      <group>
         <C.MonsterStage
           texture="textures/LavaWorld.jpg"
           position-x={-2.5}
           rotation-y={Math.PI / 8}
-          name="Dragon"
+          name={V.names.dragon}
           color="#df8252"
           active={h.active}
           setActive={h.setActive}
+          hover={h.hover}
+          setIsHover={h.setIsHover}
         >
-          <C.Dragon scale={0.46} position-y={-0.8} />
+          <C.Dragon scale={0.46} position-y={-0.8} hover={h.hover} />
         </C.MonsterStage>
         <C.MonsterStage
           texture="textures/waterEnviron.jpg"
-          name="Fish King"
+          name={V.names.fish}
           color="#50b8cd"
           active={h.active}
           setActive={h.setActive}
+          hover={h.hover}
+          setIsHover={h.setIsHover}
         >
-          <C.Fish scale={0.6} position-y={-1} />
+          <C.Fish scale={0.6} position-y={-1} hover={h.hover} />
         </C.MonsterStage>
         <C.MonsterStage
           texture="textures/cactusForest.jpg"
           position-x={2.5}
           rotation-y={-Math.PI / 8}
-          name="Cactoro"
+          name={V.names.catoro}
           color="#80aa3f"
           active={h.active}
           setActive={h.setActive}
+          hover={h.hover}
+          setIsHover={h.setIsHover}
         >
-          <C.Cactoro scale={0.4} position-y={-0.7} />
+          <C.Cactoro scale={0.4} position-y={-0.7} hover={h.hover} />
         </C.MonsterStage>
       </group>
     </>
