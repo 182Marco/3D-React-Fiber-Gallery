504035aaf4a37d27d4ae173c89aba5e29a4bf49c
Milza <marcomilza@gmail.com>
2025-02-06 18:25:29 +0100

    updated

--- src/components/dissolve/dissolve.js
+++ src/components/dissolve/dissolve.js
@@ -1,7 +1,17 @@
+import * as D from "@react-three/drei";
 import { useDissolve } from "./dissolve.hook";
+
 const Dissolve = () => {
   const h = useDissolve();
-  return <></>;
+  return (
+    <>
+      <D.OrbitControls />
+      <mesh>
+        <meshNormalMaterial />
+        <boxBufferGeometry />
+      </mesh>
+    </>
+  );
 };
 
 export { Dissolve };
