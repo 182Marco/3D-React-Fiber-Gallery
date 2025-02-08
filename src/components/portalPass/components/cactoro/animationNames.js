fa05796ea2322699570c4f4a5a8e4da234cf0d06
Milza <marcomilza@gmail.com>
2025-02-08 14:16:12 +0100

    updated animationNamesForCactoro

---
+++ src/components/portalPass/components/cactoro/animationNames.js
@@ -0,0 +1,13 @@
+const animationNames = {
+  Bite_Front: "Bite_Front",
+  Dance: "Dance",
+  Death: "Death",
+  HitRecieve: "HitRecieve",
+  Idle: "Idle",
+  Jump: "Jump",
+  No: "No",
+  Walk: "Walk",
+  Yes: "Yes",
+};
+
+export { animationNames };
