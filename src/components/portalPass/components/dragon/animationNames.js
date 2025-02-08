14914c3286a603eff73aa5334927506bcff25459
Milza <marcomilza@gmail.com>
2025-02-08 14:16:36 +0100

    updated animationNamesForDragon

---
+++ src/components/portalPass/components/dragon/animationNames.js
@@ -0,0 +1,12 @@
+const animationNames = {
+  Death: "Death",
+  Fast_Flying: "Fast_Flying",
+  Flying_Idle: "Flying_Idle",
+  Headbutt: "Headbutt",
+  HitReact: "HitReact",
+  No: "No",
+  Punch: "Punch",
+  Yes: "Yes",
+};
+
+export { animationNames };
