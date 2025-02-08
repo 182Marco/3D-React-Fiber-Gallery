f71ae7b71eb1527f5369d8d13300e12c40a9310e
Milza <marcomilza@gmail.com>
2025-02-08 14:58:12 +0100

    new project render

--- src/components/dissolve/dissolve.js
+++ src/components/dissolve/dissolve.js
@@ -1,4 +1,5 @@
 import * as D from "@react-three/drei";
+import * as C from "../../components";
 import { useDissolve } from "./dissolve.hook";
 
 const Dissolve = () => {
@@ -6,9 +7,19 @@ const Dissolve = () => {
   return (
     <>
       <D.OrbitControls />
-      <mesh>
-        <meshNormalMaterial />
-        <boxBufferGeometry />
+      <directionalLight
+        position={[-5, 5, 5]}
+        castShadow
+        shadow-mapSize-width={1024}
+      />
+      <C.Woman />
+      <mesh
+        rotation={[-0.5 * Math.PI, 0, 0]}
+        position={[0, -1, 0]}
+        receiveShadow
+      >
+        <planeBufferGeometry args={[10, 10, 1, 1]} />
+        <shadowMaterial trasparent opacity={0.2} />
       </mesh>
     </>
   );
