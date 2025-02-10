f2057543c319dba594dbdcbea13239527a978bcc
Milza <marcomilza@gmail.com>
2025-02-10 22:58:55 +0100

    removed unused

--- src/components/woman/animationNames.js
+++
@@ -1,9 +0,0 @@
-const animationNames = {
-  GuitarPlaying: "GuitarPlaying",
-  Idle: "Idle",
-  Running: "Running",
-  SalsaDancing: "SalsaDancing",
-  TPose: "TPose",
-};
-
-export { animationNames };
