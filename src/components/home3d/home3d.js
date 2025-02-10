fd53d9cebba439c7af2d482c3ddc661ab0444dc5
Milza <marcomilza@gmail.com>
2025-02-10 22:51:54 +0100

    home 3d render updated

--- src/components/home3d/home3d.js
+++ src/components/home3d/home3d.js
@@ -1,16 +1,20 @@
 import * as D from "@react-three/drei";
 import * as C from "..";
 import J from "./textsToPass.json";
+import { useHome3d } from "./home3d.hook";
 
-const Home3d = () => (
-  <>
-    <ambientLight intensity={1} />
-    <D.OrbitControls enableZoom={false} />
-    <D.ScrollControls pages={3} damping={0.25}>
-      <C.OverlayText txt={J.txt} />
-      <C.Office scale={1.3} />
-    </D.ScrollControls>
-  </>
-);
+const Home3d = () => {
+  const h = useHome3d();
+  return (
+    <>
+      <ambientLight intensity={1} />
+      <D.CameraControls enabled={false} ref={h.cameraRef} />
+      <D.ScrollControls pages={3} damping={0.25}>
+        <C.OverlayText txt={J.txt} />
+        <C.Office scale={1.3} name={h.name} />
+      </D.ScrollControls>
+    </>
+  );
+};
 
 export { Home3d };
