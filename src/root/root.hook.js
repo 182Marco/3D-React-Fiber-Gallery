15c09b54814f1c64ce01d2ea64594f441c0f6235
Milza <marcomilza@gmail.com>
2025-02-11 15:59:06 +0100

    updated root

--- src/root/root.hook.js
+++ src/root/root.hook.js
@@ -6,7 +6,7 @@ const useRoot = () => {
   const options = Object.keys(U.projects);
 
   const { project } = L.useControls({
-    project: { value: U.projects.PortalPass, options },
+    project: { value: U.projects.Woman, options },
   });
 
   const [currentProject, setCurrentProject] = R.useState(project);
@@ -15,7 +15,18 @@ const useRoot = () => {
     setCurrentProject(project);
   }, [project]);
 
-  return { currentProject, project };
+  const getBgClass = project => {
+    switch (project) {
+      case U.projects.PortalPass:
+        return "bg dark-bg";
+      case U.projects.Woman:
+        return "bg grey-bg";
+      default:
+        return "bg light-bg";
+    }
+  };
+
+  return { currentProject, getBgClass, project };
 };
 
 export { useRoot };
