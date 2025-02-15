92f33f4492c760544cc450c56c7ef511ce0a7c24
Milza <marcomilza@gmail.com>
2025-02-15 19:12:51 +0100

    created new project component

---
+++ src/components/cartFollowingMidget/cartFollowingMidget.js
@@ -0,0 +1,29 @@
+import * as C from "./components";
+import * as D from "@react-three/drei";
+import { useCartFollowingMidget } from "./cartFollowingMidget.hook";
+
+const CartFollowingMidget = () => {
+  const h = useCartFollowingMidget();
+  return (
+    <>
+      <D.OrbitControls />
+      <ambientLight />
+      <group rotation={[0, Math.PI / 12, 0]} position={[0, -0.7, 0]}>
+        <C.LampPost />
+        <C.Rock />
+        <C.Ankou
+          scale={3}
+          rotation={[0, (-Math.PI * 4) / 10, 0]}
+          position={[-1, -1.7, -1]}
+        />
+        <C.YoungKorrigan
+          scale={1.3}
+          position={[3.5, -1, -1]}
+          rotation={[0, (-Math.PI * 4) / 10, 0]}
+        />
+      </group>
+    </>
+  );
+};
+
+export { CartFollowingMidget };
