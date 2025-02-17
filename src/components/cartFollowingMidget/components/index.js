b60572963a6f1bb1e8aaa900abd1eb6108ca8cf1
Milza <marcomilza@gmail.com>
2025-02-17 19:54:33 +0100

    new component for kurrigan exported

--- src/components/cartFollowingMidget/components/index.js
+++ src/components/cartFollowingMidget/components/index.js
@@ -1,4 +1,5 @@
 export { Ankou } from "./ankou";
 export { LampPost } from "./lampPost";
-export { YoungKorrigan } from "./youngKorrigon";
 export { Rock } from "./rock";
+export { Tree } from "./tree";
+export { YoungKorrigan } from "./youngKorrigon";
