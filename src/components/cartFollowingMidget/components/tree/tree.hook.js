ff2a49b449ee7810f169bf18f658b56cd9639e4d
Milza <marcomilza@gmail.com>
2025-02-17 19:46:50 +0100

    updated import

--- src/components/cartFollowingMidget/components/tree/tree.hook.js
+++ src/components/cartFollowingMidget/components/tree/tree.hook.js
@@ -1,9 +1,9 @@
-import * as D from "@rea ct-three/drei";
+import * as D from "@react-three/drei";
 import * as R from "react";
 
 const useTree = () => {
   const group = R.useRef();
-  const { nodes, materials } = D.useGLTF("./models/tree-spruce/model.gltf");
+  const { nodes, materials } = D.useGLTF("./model/tree.gltf");
   return { group, nodes, materials };
 };
 
