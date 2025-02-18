6472c3eb3dee7624fcb030042e11fee3fe4398b2
Milza <marcomilza@gmail.com>
2025-02-18 20:06:36 +0100

    work in progress for cart rendering

---
+++ src/components/cartFollowingMidget/cartFollowingMidget.updateCamera.js
@@ -0,0 +1,21 @@
+import * as R from "react";
+import * as T from "@react-three/fiber";
+
+const useUpdateCamera = () => {
+  const { camera } = T.useThree();
+
+  R.useEffect(() => {
+    const originalPosition = camera.position.clone();
+    const originalFov = camera.fov;
+
+    camera.position.set(-5, 1, 6);
+    camera.fov = 25;
+    camera.updateProjectionMatrix();
+
+    return () => {
+      camera.position.copy(originalPosition);
+      camera.fov = originalFov;
+      camera.updateProjectionMatrix();
+    };
+  }, [camera]);
+};
