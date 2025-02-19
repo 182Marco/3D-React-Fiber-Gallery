660653facd6ff70c4436c383f28e5a55c5bfbb9b
Milza <marcomilza@gmail.com>
2025-02-19 17:31:58 +0100

    logic to move lampPost

--- src/components/cartFollowingMidget/components/movingItem/movingItem.hook.js
+++ src/components/cartFollowingMidget/components/movingItem/movingItem.hook.js
@@ -1,7 +1,13 @@
+import * as F from "@react-three/fiber";
 import * as R from "react";
 
 const useMovingItem = () => {
   const groupRef = R.useRef();
+
+  F.useFrame((_, d) => {
+    groupRef.current.position.x += d;
+  });
+
   return { groupRef };
 };
 
