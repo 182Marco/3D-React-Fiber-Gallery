0cafa1601d48b1b80ae3237e6352d33970f8984e
Milza <marcomilza@gmail.com>
2025-02-19 18:14:23 +0100

    looping bg

--- src/components/cartFollowingMidget/components/movingItem/movingItem.hook.js
+++ src/components/cartFollowingMidget/components/movingItem/movingItem.hook.js
@@ -1,11 +1,15 @@
 import * as F from "@react-three/fiber";
 import * as R from "react";
+import { vars } from "./fixedVars";
 
 const useMovingItem = () => {
   const groupRef = R.useRef();
 
   F.useFrame((_, d) => {
-    groupRef.current.position.x += d;
+    groupRef.current.position.x += d * 5;
+    if (groupRef.current.position.x >= vars.lampPost_max_offset_X) {
+      groupRef.current.position.x = 3;
+    }
   });
 
   return { groupRef };
