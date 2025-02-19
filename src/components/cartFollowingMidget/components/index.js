22c3191a53c86cb434bffd857558293fc7ffaf3f
Milza <marcomilza@gmail.com>
2025-02-19 17:24:08 +0100

    new comp in cart following midget export

--- src/components/cartFollowingMidget/components/index.js
+++ src/components/cartFollowingMidget/components/index.js
@@ -1,5 +1,6 @@
 export { Ankou } from "./ankou";
 export { LampPost } from "./lampPost";
+export { MovingItem } from "./movingItem";
 export { Rock } from "./rock";
 export { Tree } from "./tree";
 export { YoungKorrigan } from "./youngKorrigon";
