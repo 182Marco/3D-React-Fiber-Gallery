97e329bbede735d4285666e142ff9bd866d8e68b
Milza <marcomilza@gmail.com>
2025-02-20 20:51:51 +0100

    updated cart hook wip

--- src/components/cartFollowingMidget/cartFollowingMidget.hook.js
+++ src/components/cartFollowingMidget/cartFollowingMidget.hook.js
@@ -5,8 +5,7 @@ const useCartFollowingMidget = () => {
   useUpdateCamera();
 
   const getPosition = i =>
-    -vars.lampPost_max_offset_X +
-    (2 * vars.lampPost_max_offset_X * i) / (vars.lampPost_N - 1);
+    -vars.lampMaxX + (2 * vars.lampMaxX * i) / (vars.lampN - 1);
 
   return { getPosition };
 };
