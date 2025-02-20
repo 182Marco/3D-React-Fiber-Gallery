ee42c3091881adb7eda8ca3cf359079ccdd1970f
Milza <marcomilza@gmail.com>
2025-02-20 20:18:08 +0100

    updated cart following logic

--- src/components/cartFollowingMidget/cartFollowingMidget.hook.js
+++ src/components/cartFollowingMidget/cartFollowingMidget.hook.js
@@ -1,11 +1,14 @@
-import * as R from "react";
-import * as T from "@react-three/fiber";
 import { useUpdateCamera } from "./cartFollowingMidget.updateCamera";
+import { vars } from "./fixedVars";
 
 const useCartFollowingMidget = () => {
   useUpdateCamera();
 
-  return {};
+  const getPosition = i =>
+    -vars.lampPost_max_offset_X +
+    (2 * vars.lampPost_max_offset_X * i) / (vars.lampPost_N - 1);
+
+  return { getPosition };
 };
 
 export { useCartFollowingMidget };
