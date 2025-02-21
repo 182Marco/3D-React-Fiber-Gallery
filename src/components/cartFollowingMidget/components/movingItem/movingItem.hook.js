858d93d2dd1d6adc2091bb85b5ea166acd453e08
Milza <marcomilza@gmail.com>
2025-02-21 17:34:21 +0100

    updated speed for lampPosts

--- src/components/cartFollowingMidget/components/movingItem/movingItem.hook.js
+++ src/components/cartFollowingMidget/components/movingItem/movingItem.hook.js
@@ -6,7 +6,7 @@ const useMovingItem = () => {
   const groupRef = R.useRef();
 
   F.useFrame((_, d) => {
-    groupRef.current.position.x += d * 5;
+    groupRef.current.position.x += d / 2;
     if (groupRef.current.position.x >= vars.lampMaxX) {
       groupRef.current.position.x = -vars.lampMaxX;
     }
