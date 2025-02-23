d4ebbda22cb7a7638012d208588bc2c96964431d
Milza <marcomilza@gmail.com>
2025-02-23 19:34:43 +0100

    more parametrical

--- src/components/cartFollowingMidget/components/movingItem/movingItem.js
+++ src/components/cartFollowingMidget/components/movingItem/movingItem.js
@@ -1,9 +1,9 @@
 import { useMovingItem } from "./movingItem.hook";
 
-const MovingItem = props => {
-  const h = useMovingItem(props.xSpread);
+const MovingItem = ({ xSpread, speed, children }) => {
+  const h = useMovingItem(xSpread, speed);
 
-  return <group ref={h.groupRef}>{props.children}</group>;
+  return <group ref={h.groupRef}>{children}</group>;
 };
 
 export { MovingItem };
