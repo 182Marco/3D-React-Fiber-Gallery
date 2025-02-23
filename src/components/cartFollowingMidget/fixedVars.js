a00bc9b5b633aa4e4fd7035b26f65761548d6e52
Milza <marcomilza@gmail.com>
2025-02-23 17:50:39 +0100

    updated vars

--- src/components/cartFollowingMidget/fixedVars.js
+++ src/components/cartFollowingMidget/fixedVars.js
@@ -1,14 +1,11 @@
 const vars = {
   //lampMaxX: 6.43,
-  //
-  /*   lampMaxX: 30,
-  lampN: 24, */
   lampMaxX: 30,
-  lampN: 18,
+  lampN: 12,
   rocksMaxX: 40,
-  rocksN: 10,
+  rocksN: 4,
   treeMaxX: 27,
-  treeN: 22,
+  treeN: 7,
 };
 
 export { vars };
