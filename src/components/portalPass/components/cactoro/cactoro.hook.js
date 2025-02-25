2275f522b2f8b73edea93a13ec6efc604eafa662
Milza <marcomilza@gmail.com>
2025-02-25 11:06:09 +0100

    less code

--- src/components/portalPass/components/cactoro/cactoro.hook.js
+++ src/components/portalPass/components/cactoro/cactoro.hook.js
@@ -3,18 +3,19 @@ import * as F from "@react-three/fiber";
 import * as D from "@react-three/drei";
 import * as V from "../../environmentVars";
 import { SkeletonUtils } from "three-stdlib";
-import { animationNames } from "./animationNames";
 
 const useCactoro = hover => {
   const group = R.useRef();
   const { scene, animations } = D.useGLTF("./model/cactoro.gltf");
   const clone = R.useMemo(() => SkeletonUtils.clone(scene), [scene]);
   const { nodes, materials } = F.useGraph(clone);
-  const { actions } = D.useAnimations(animations, group);
+  const { actions, names } = D.useAnimations(animations, group);
 
   R.useEffect(() => {
     const animationName =
-      hover === V.names.catoro ? animationNames.Dance : animationNames.Idle;
+      hover === V.names.catoro
+        ? names[names.indexOf("Dance")]
+        : names[names.indexOf("Idle")];
     const action = actions[animationName];
     if (action) {
       action.reset().fadeIn(0.5).play();
