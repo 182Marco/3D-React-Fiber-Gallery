13fc589cd1c1beb9d6d3ef2728da3ab05779fcb4
Milza <marcomilza@gmail.com>
2025-02-25 11:08:50 +0100

    working with less code

--- src/components/portalPass/components/dragon/dragon.hook.js
+++ src/components/portalPass/components/dragon/dragon.hook.js
@@ -3,20 +3,19 @@ import * as F from "@react-three/fiber";
 import * as D from "@react-three/drei";
 import * as V from "../../environmentVars";
 import { SkeletonUtils } from "three-stdlib";
-import { animationNames } from "./animationNames";
 
 const useDragon = hover => {
   const group = R.useRef();
   const { scene, animations } = D.useGLTF("./model/dragon.gltf");
   const clone = R.useMemo(() => SkeletonUtils.clone(scene), [scene]);
   const { nodes, materials } = F.useGraph(clone);
-  const { actions } = D.useAnimations(animations, group);
+  const { actions, names } = D.useAnimations(animations, group);
 
   R.useEffect(() => {
     const animation =
       hover === V.names.dragon
-        ? animationNames.Headbutt
-        : animationNames.Flying_Idle;
+        ? names[names.indexOf("Headbutt")]
+        : names[names.indexOf("Flying_Idle")];
     actions[animation].reset().fadeIn(0.5).play();
     return () => actions[animation]?.stop();
   }, [hover, actions]);
