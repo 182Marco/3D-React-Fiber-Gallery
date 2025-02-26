b3403b29484e12be3f4a8c90bc1d8b4259a143bd
Milza <marcomilza@gmail.com>
2025-02-26 11:02:56 +0100

    working again childrenComponentProps

--- src/components/cartFollowingMidget/childrenCompontsProps.js
+++ src/components/cartFollowingMidget/childrenCompontsProps.js
@@ -3,52 +3,51 @@ import { vars } from "./fixedVars";
 
 const getPosition = (i, xMax, number) => -xMax + (2 * xMax * i) / (number - 1);
 
-const getRocks = i => ({
-  position: [
-    getPosition(i, vars.rocks.xMax, vars.rocks.quantity),
-    0,
-    U.getRandomIntExceptRange({
-      range: vars.rocks.rangePosition,
-      excludeRanges: [vars.subjectZPositionRange, vars.lamps.zPositionRange],
-    }),
-  ],
-  scale: U.getRandomWithin(...vars.rocks.scaleRange),
-});
+const getZPosition = name => {
+  switch (name) {
+    case "rocks":
+      return () =>
+        U.getRandomIntExceptRange({
+          range: vars[name].rangePosition,
+          excludeRanges: [
+            vars.subjectZPositionRange,
+            vars.lamps.zPositionRange,
+          ],
+        });
+    case "lamps":
+      return () => vars.lamps.zPosition;
+    default:
+      return () => U.getRandomWithin(...vars[name].rangePosition);
+  }
+};
 
-const getTrees = i => ({
+const getProps = name => i => ({
   position: [
-    getPosition(i, vars.trees.xMax, vars.trees.quantity),
+    getPosition(i, vars[name].xMax, vars[name].quantity),
     0,
-    U.getRandomWithin(...vars.trees.rangePosition),
+    getZPosition(name)(),
   ],
-  scale: U.getRandomWithin(...vars.trees.scaleRange),
-});
-
-const getLamps = i => ({
-  position: [
-    getPosition(i, vars.lamps.xMax, vars.lamps.quantity),
-    0,
-    vars.lamps.zPosition,
-  ],
-  scale: vars.lamps.scale,
+  scale: vars[name]?.scale
+    ? vars[name].scale
+    : U.getRandomWithin(...vars[name].scaleRange),
 });
 
 const lampsProps = {
-  getMeshComponentProps: getLamps,
+  getMeshComponentProps: getProps("lamps"),
   count: vars.lamps.quantity,
   xSpread: vars.lamps.xMax,
   speed: vars.lamps.speed,
 };
 
 const treeProps = {
-  getMeshComponentProps: getTrees,
+  getMeshComponentProps: getProps("trees"),
   count: vars.trees.quantity,
   xSpread: vars.trees.xMax,
   speed: vars.trees.speed,
 };
 
 const rockProps = {
-  getMeshComponentProps: getRocks,
+  getMeshComponentProps: getProps("rocks"),
   count: vars.rocks.quantity,
   xSpread: vars.rocks.xMax,
   speed: vars.rocks.speed,
