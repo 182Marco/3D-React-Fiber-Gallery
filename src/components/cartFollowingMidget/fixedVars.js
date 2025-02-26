6d4a5dd03eec4e7efa73e257249b2a201a1ddd46
Milza <marcomilza@gmail.com>
2025-02-26 10:02:27 +0100

    updated fixed vars

--- src/components/cartFollowingMidget/fixedVars.js
+++ src/components/cartFollowingMidget/fixedVars.js
@@ -1,20 +1,27 @@
 const vars = {
-  rockMaxX: 13,
-  rockN: 9,
-  rockSpeed: 0.75,
-  rockRangePosition: [-4.5, 4],
-  lampMaxX: 30,
-  lampN: 8,
-  lampSpeed: 0.75,
-  lampScale: 0.7,
-  lampZpoistion: -2,
-  treeMaxX: 27,
-  treeN: 40,
-  treeSpeed: 0.63,
-  treeScaleRange: [0.09, 0.14],
-  treesRangePosition: [-13, -4],
+  rocks: {
+    xMax: 13,
+    quantity: 9,
+    speed: 0.75,
+    rangePosition: [-4.5, 4],
+    scaleRange: [0.06, 0.016],
+  },
+  lamps: {
+    xMax: 30,
+    quantity: 8,
+    speed: 0.75,
+    scale: 0.7,
+    zPosition: -2,
+    zPositionRange: [-2.2, -1.8],
+  },
+  trees: {
+    xMax: 27,
+    quantity: 40,
+    speed: 0.63,
+    scaleRange: [0.09, 0.14],
+    rangePosition: [-13, -4],
+  },
   subjectZPositionRange: [-0.9, 0.9],
-  lampZPositionRange: [-2.2, -1.8],
 };
 
 export { vars };
