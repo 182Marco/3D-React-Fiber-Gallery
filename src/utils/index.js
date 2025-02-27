6e1e3c579b29d1a46d89b2d7407033d7ba43c51b
Milza <marcomilza@gmail.com>
2025-02-27 16:45:25 +0100

    exported new utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,4 @@
 export { createSeqArray } from "./WorkOnArrays";
+export { getRandomIntExceptRange, getRandomWithin } from "./randomNumbers";
 export { purify } from "./purify";
+export { wait } from "./wait";
