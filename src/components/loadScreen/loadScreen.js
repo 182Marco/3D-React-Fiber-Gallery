60cdbc4d1b0bd3befd480845ed4f1a7b6de99e50
Milza <marcomilza@gmail.com>
2025-03-01 20:37:33 +0100

    new load screen component

---
+++ src/components/loadScreen/loadScreen.js
@@ -0,0 +1,28 @@
+import { useLoadScreen } from "./loadScreen.hook";
+import "./styles.scss";
+
+const LoadScreen = ({ started, onStarted }) => {
+  const h = useLoadScreen();
+  return (
+    <div className={`loadingScreen ${started ? "start" : ""}`}>
+      <h1>The Carriage of Destiny!</h1>
+      <main>
+        <button disabled={h.progress < 100} onClick={onStarted}>
+          Start
+        </button>
+      </main>
+      {!h.startedExperience && (
+        <section>
+          <span>{h.progress.toFixed(0)}%</span>
+          <div
+            style={{
+              width: `${h.progress}%`,
+            }}
+          />
+        </section>
+      )}
+    </div>
+  );
+};
+
+export { LoadScreen };
