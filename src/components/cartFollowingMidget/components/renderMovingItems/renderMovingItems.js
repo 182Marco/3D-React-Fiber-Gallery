c6f4a2c12fc784bec4733a01b41f9cd6bdb7c91b
Milza <marcomilza@gmail.com>
2025-03-01 20:40:40 +0100

    updated component renderMovingItems

--- src/components/cartFollowingMidget/components/renderMovingItems/renderMovingItems.js
+++ src/components/cartFollowingMidget/components/renderMovingItems/renderMovingItems.js
@@ -1,17 +1,27 @@
 import * as C from "../../components";
+import * as U from "./utils";
+import { vars } from "./fixedVars";
 
-const RenderMovingItems = ({
-  Component,
-  getMeshComponentProps,
-  count,
-  xSpread,
-  speed,
-}) => {
-  return [...Array(count)].map((_, i) => (
-    <C.MovingItem xSpread={xSpread} speed={speed} key={i}>
-      <Component {...getMeshComponentProps(i)} />
-    </C.MovingItem>
-  ));
-};
-
+const RenderMovingItems = ({ name }) => (
+  <>
+    {[...Array(vars[name].quantity)].map((_, i) => (
+      <C.MovingItem
+        xSpread={vars[name].xSpread}
+        speed={vars[name].speed}
+        key={i}
+      >
+        {(() => {
+          switch (name) {
+            case "rocks":
+              return <C.Rock {...U.getSingleMeshProps(name, i)} />;
+            case "lamps":
+              return <C.LampPost {...U.getSingleMeshProps(name, i)} />;
+            default:
+              return <C.Tree {...U.getSingleMeshProps(name, i)} />;
+          }
+        })()}
+      </C.MovingItem>
+    ))}
+  </>
+);
 export { RenderMovingItems };
