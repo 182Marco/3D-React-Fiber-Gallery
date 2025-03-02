f0e05c58e5a018a1a58c5e6343fb773dd7c94075
Milza <marcomilza@gmail.com>
2025-03-02 20:53:17 +0100

    added project

---
+++ src/scene/utils/projects.js
@@ -0,0 +1,12 @@
+const projects = {
+  Bike: "Bike",
+  CartFollowingMidget: "CartFollowingMidget",
+  Dog: "Dog",
+  Explode: "Explode",
+  Home3d: "Home3d",
+  PortalPass: "PortalPass",
+  slideShow: "slideShow",
+  Woman: "Woman",
+};
+
+export { projects };
