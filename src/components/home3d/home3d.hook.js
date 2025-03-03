45ada36c39baf71e06d714c04cc024ff09962989
Milza <marcomilza@gmail.com>
2025-03-03 06:46:18 +0100

    updated camera for home 3d

--- src/components/home3d/home3d.hook.js
+++ src/components/home3d/home3d.hook.js
@@ -1,45 +1,28 @@
 import * as R from "react";
 import * as F from "@react-three/fiber";
-import * as THREE from "three";
 
 const useHome3d = () => {
   const name = "home3d";
-  const cameraRef = R.useRef();
-  const scene = F.useThree(state => state.scene);
-
   const { camera } = F.useThree();
 
-  R.useEffect(() => {
-    const originalPosition = camera.position.clone();
-    const originalFov = camera.fov;
+  const previusPosition = camera.position.clone();
+  const priviusFov = camera.fov;
 
-    camera.position.set(0, 5, 20);
-    camera.fov = 25;
+  R.useEffect(() => {
+    camera.position.set(0, 4, 5);
+    camera.fov = 75;
     camera.updateProjectionMatrix();
 
     return () => {
-      camera.position.copy(originalPosition);
-      camera.fov = originalFov;
+      camera.position.set(
+        previusPosition.x,
+        previusPosition.y,
+        previusPosition.z,
+      );
+      camera.fov = priviusFov;
       camera.updateProjectionMatrix();
     };
   }, [camera]);
-
-  R.useEffect(() => {
-    if (!cameraRef.current) return;
-
-    const targetPosition = new THREE.Vector3();
-    scene.getObjectByName(name)?.getWorldPosition(targetPosition);
-
-    cameraRef.current.setLookAt(
-      0,
-      0,
-      -12,
-      targetPosition.x,
-      targetPosition.y,
-      targetPosition.z,
-      true,
-    );
-  }, [scene, name]);
 
   return {
     name,
