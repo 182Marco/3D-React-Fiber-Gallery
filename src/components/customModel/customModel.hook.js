fb63c6651284e4cb76524d6b9f901da66d9c65e2
Milza <marcomilza@gmail.com>
2025-03-04 16:51:00 +0100

    updated path

--- src/components/customModel/customModel.hook.js
+++ src/components/customModel/customModel.hook.js
@@ -2,7 +2,7 @@ import { useLoader } from "@react-three/fiber";
 import * as THREE from "three";
 
 const useCustomModel = () => ({
-  texture: useLoader(THREE.TextureLoader, "./img/1.png"),
+  texture: useLoader(THREE.TextureLoader, "./images/1.png"),
   positionArray: new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]),
   uvCoordinates: new Float32Array([0, 0, 0, 1, 0]),
 });
