28e1390eb4bbfb638a80dd305e6e00c5796ffdc7
Milza <marcomilza@gmail.com>
2025-03-07 19:24:18 +0100

    updated logic for loadScreen

--- src/components/loadScreen/loadScreen.hook.js
+++ src/components/loadScreen/loadScreen.hook.js
@@ -1,10 +1,23 @@
+import * as R from "react";
 import { useProgress } from "@react-three/drei";
 import { useAppContext } from "../../context";
+import { wait } from "@testing-library/user-event/dist/utils";
 
 const useLoadScreen = () => {
   const { startedExperience } = useAppContext();
   const { progress } = useProgress();
-  return { progress, startedExperience };
+  const [removeElement, setRemoveElement] = R.useState(false);
+
+  const handleRemoveElement = async () => {
+    await wait(3500);
+    setRemoveElement(true);
+  };
+
+  R.useEffect(() => {
+    if (startedExperience) handleRemoveElement();
+  }, [startedExperience]);
+
+  return { progress, removeElement, startedExperience };
 };
 
 export { useLoadScreen };
