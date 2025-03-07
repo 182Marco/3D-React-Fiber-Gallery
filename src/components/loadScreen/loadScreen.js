61611678ed6c720f2799017467c25f2823a644de
Milza <marcomilza@gmail.com>
2025-03-07 19:23:43 +0100

    load screen updated

--- src/components/loadScreen/loadScreen.js
+++ src/components/loadScreen/loadScreen.js
@@ -4,24 +4,28 @@ import "./styles.scss";
 const LoadScreen = ({ started, onStarted }) => {
   const h = useLoadScreen();
   return (
-    <div className={`loadingScreen ${started ? "start" : ""}`}>
-      <h1>The Carriage of Destiny!</h1>
-      <main>
-        <button disabled={h.progress < 100} onClick={onStarted}>
-          Start
-        </button>
-      </main>
-      {!h.startedExperience && (
-        <section>
-          <span>{h.progress.toFixed(0)}%</span>
-          <div
-            style={{
-              width: `${h.progress}%`,
-            }}
-          />
-        </section>
+    <>
+      {!h.removeElement && (
+        <div className={`loadingScreen ${started ? "start" : ""}`}>
+          <h1>The Carriage of Destiny!</h1>
+          <main>
+            <button disabled={h.progress < 100} onClick={onStarted}>
+              Start
+            </button>
+          </main>
+          {!h.startedExperience && (
+            <section>
+              <span>{h.progress.toFixed(0)}%</span>
+              <div
+                style={{
+                  width: `${h.progress}%`,
+                }}
+              />
+            </section>
+          )}
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
